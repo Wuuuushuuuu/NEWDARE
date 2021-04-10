0dbf1147d49a1a72c98912a89af467eec37609ad
Wuuuushuuuu <Jon_mananghaya@dlsu.edu.ph>
2021-04-10 11:55:05 +0800

    Create login.py

---
+++ login.py
@@ -0,0 +1,10 @@
+print("Hello World ") ##testing 
+print("Test #2")##Hello!
+##Yooooo wassup !!
+<<<<<<< HEAD
+print("Hello!")
+=======
+##Testing pt.2
+>>>>>>> c72da233925c4bdcf84f8baa1dc39536f8118165
+print("Wassup!")
+print(Hello!)
