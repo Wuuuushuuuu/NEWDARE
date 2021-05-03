68518e866202a55c9728647c89c85443acee9d94
Wuuuushuuuu <Jon_mananghaya@dlsu.edu.ph>
2021-05-03 09:02:41 +0800

    Dare 5

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
-def Login_view(request, *args, **kwargs):
+def Login_view_dentist(request, *args, **kwargs):
      return render(request, "Login.html", {})
 
 def Contact_view(request, *args, **kwargs):
      return render(request, "Contact.html", {})
+
+def Login_view_client(request, *args, **kwargs):
+     return render(request, "User_Login.html", {})
+
+def Register_view(request, *args, **kwargs):
+     return render(request, "Register.html", {})
