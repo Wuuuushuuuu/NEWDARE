b6284d16142e0ee5eb40b6b38687d59b38950de4
Wuuuushuuuu <Jon_mananghaya@dlsu.edu.ph>
2021-05-24 03:33:41 +0800

    fianlly some fucking hope hahaha

--- pages/views.py
+++ pages/views.py
@@ -14,31 +14,29 @@ def Login_view_dentist(request, *args, **kwargs):
      return render(request, "Login.html", {})
 
 def Contact_view(request, *args, **kwargs):
-     return redirect('https://www.youtube.com/watch?v=ekyCxgtW3Js&ab_channel=DennisIvyDennisIvyVerified')
+     return redirect('Login')
 
 def Login_view_client(request):
      if request.method == 'POST':
           name = request.POST.get('name')
           contact_no = request.POST.get('contact_no')
 
-          #count = client.objects.all().count()
+          count = client.objects.all().count()
+          
           return  redirect('app:home')
      
      return render(request, "User_Login.html", {})
 
 def Register_view(request):
      
-     form = SaveClientRecord()
+     form = SaveClientRecord(request.POST)
 
      if request.method =='POST':
-         name = request.POST['name']
-         email = request.POST['email']
-         contact_no = request.POST['contact_no']
-         client = Records(Name=name,Email=email,Contact_no = contact_no)
-         client.save()
-         return redirect('home')
-     else:
-          return render(request, "Register.html")
+          print(request.method)
+          if form.is_valid():
+               form.save()
+               return redirect('/ home/')
+
           
 
      context = {'form': form} 
