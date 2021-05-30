581e99add0ec39d8ae2bb28ae4c3dd36633be80e
Wuuuushuuuu <Jon_mananghaya@dlsu.edu.ph>
2021-05-31 01:00:14 +0800

    yes

--- web_project/settings.py
+++ web_project/settings.py
@@ -59,6 +59,9 @@ SPECIAL_DISABLED = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +146,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
